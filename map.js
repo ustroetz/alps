2faeb5ff8bfc0e71895d6963645852635a0f2dcc
Ulrich Stroetz <ustroetz@gmail.com>
2015-09-13 16:18:16 +0200

    Add directions

--- map.js
+++ map.js
@@ -4,22 +4,40 @@ var map = L.mapbox.map('map', 'mapbox.streets', {
 }).setView([40, -74.50], 9);
 
 var stopsLayer = L.geoJson(stops, {
-	style: function(feature) { return feature.properties; },
+    style: function(feature) {
+        return feature.properties;
+    },
     onEachFeature: function(feature, layer) {
-    	popupContent = "<b>" + feature.properties.name + "</b><br>" + feature.properties.description
+        popupContent = "<b>" + feature.properties.name + "</b><br>" + feature.properties.description
         layer.bindPopup(popupContent);
     }
 })
 
-
-
 var directions = L.mapbox.directions();
+var directionsLayer = L.mapbox.directions.layer(directions)
+    .addTo(map);
+var directionsRoutesControl = L.mapbox.directions.routesControl('routes', directions);
 
-// var directionsLayer = L.mapbox.directions.layer(directions)
-//     .addTo(map);
+var index = -1
+stopsLayer.eachLayer(function(layer) {
+    var type = layer.feature.properties.type;
+    var coords = layer.feature;
+    if (type == 'origin') {
+        directions.setOrigin(coords);
+    } else if (type == 'destination') {
+        directions.setDestination(coords)
+    } else if (type == 'waypoint') {
+        directions.setWaypoint(index, coords)
+    };
+    index += 1;
+});
+
+if (directions.queryable()) {
+    console.log(directions);
+    directions.query();
+}
 
 
 
 stopsLayer.addTo(map);
 map.fitBounds(stopsLayer.getBounds());
-
