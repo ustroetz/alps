c405ad8a0e43e5677b56e3000bce6be575f3d100
Ulrich Stroetz <ustroetz@gmail.com>
2015-09-13 15:36:52 +0200

    Initial upload

---
+++ map.js
@@ -0,0 +1,25 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiYWxscnlkZXIiLCJhIjoidWs5cUFfRSJ9.t8kxvO3nIhCaAl07-4lkNw';
+var map = L.mapbox.map('map', 'mapbox.streets', {
+    zoomControl: true
+}).setView([40, -74.50], 9);
+
+var stopsLayer = L.geoJson(stops, {
+	style: function(feature) { return feature.properties; },
+    onEachFeature: function(feature, layer) {
+    	popupContent = "<b>" + feature.properties.name + "</b><br>" + feature.properties.description
+        layer.bindPopup(popupContent);
+    }
+})
+
+
+
+var directions = L.mapbox.directions();
+
+var directionsLayer = L.mapbox.directions.layer(directions)
+    .addTo(map);
+
+
+map.fitBounds(stopsLayer.getBounds());
+
+stopsLayer.addTo(map);
+
